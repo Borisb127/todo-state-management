6e7f62fe50c7f0fa7101c05f7b63a713102b516d
Borisb127 <boris.borochov92@gmail.com>
2026-05-14 20:30:55 +0300

    Add Redux store with todos state management

---
+++ store/actions/todo-actions.js
@@ -0,0 +1,22 @@
+import { todoService } from '../../services/todo.service.js'
+import { store, SET_TODOS, REMOVE_TODO, ADD_TODO, UPDATE_TODO } from '../store.js'
+
+
+export function loadTodos(filterBy = {}) {
+    return todoService.query(filterBy)
+        .then(todos => store.dispatch({ type: SET_TODOS, todos }))
+}
+
+export function removeTodo(todoId) {
+    return todoService.remove(todoId)
+        .then(() => store.dispatch({ type: REMOVE_TODO, todoId }))
+}
+
+export function saveTodo(todo) {
+    const type = todo._id ? UPDATE_TODO : ADD_TODO
+    return todoService.save(todo)
+        .then(savedTodo => {
+            store.dispatch({ type, todo: savedTodo })
+            return savedTodo
+        })
+}
---
+++ store/store.js
@@ -0,0 +1,38 @@
+const { createStore } = Redux
+
+const initialState = {
+    todos: [],
+}
+
+export const SET_TODOS = 'SET_TODOS'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const ADD_TODO = 'ADD_TODO'
+export const UPDATE_TODO = 'UPDATE_TODO'
+
+
+function appReducer(state = initialState, cmd) {
+    switch (cmd.type) {
+        case SET_TODOS:
+            return { ...state, todos: cmd.todos }
+
+
+        case REMOVE_TODO:
+            var todos = state.todos.filter(todo => todo._id !== cmd.todoId)
+            return { ...state, todos }
+
+        case ADD_TODO:
+            var todos = [...state.todos, cmd.todo]
+            return { ...state, todos }
+
+        case UPDATE_TODO:
+            var todos = state.todos.map(todo => todo._id === cmd.todo._id ? cmd.todo : todo)
+            return { ...state, todos }
+
+        default:
+            return state
+    }
+}
+
+export const store = createStore(appReducer)
+
+window.gStore = store
